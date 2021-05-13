7d78eead537128c8b32238c793e5c68a24e14111
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-05-13 00:06:44 -0700

    Codigo de gato

--- JuegoDelGatito/Form1.cs
+++ JuegoDelGatito/Form1.cs
@@ -61,10 +61,110 @@ namespace JuegoDelGatito
                     FichaJuego.Dock = DockStyle.Fill;
                     FichaJuego.Cursor = Cursors.Hand;
                     FichaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    FichaJuego.Click += Jugar;
                     FichasGato.Controls.Add(FichaJuego, j, i);
                     Gato[i, j] = 0;
                 }
             }
+        }
+        private void Jugar(object sender, EventArgs e)
+        {
+            var FichaSeleccionadaUsuario = (PictureBox)sender;
+            FichaSeleccionadaUsuario.Enabled = false;
+            FichaSeleccionadaUsuario.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+            string[] Posicion = FichaSeleccionadaUsuario.Name.Split("_".ToCharArray());
+            int Fila = Convert.ToInt32(Posicion[0]);
+            int Columna = Convert.ToInt32(Posicion[1]);
+            Gato[Fila, Columna] = turno;
+            verificarJuego(Fila, Columna);
+            turno = (turno == 1) ? 2 : 1;
+        }
+        public void verificarJuego(int Fila, int Columna)
+        {
+            int GanoFilas = 0;
+            int GanoColumnas = 0;
+            int DiagonalPrincipal = 0;
+            int DiagonalInversa = 0;
+            int TamanioGato = 3;
+
+            for (var i = 0; i < TamanioGato; i++)
+            {
+                for (var j = 0; j < TamanioGato; j++)
+                {
+                    if (i == Fila)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoFilas++;
+                        }
+                    }
+
+                    if (j == Columna)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoColumnas++;
+                        }
+                    }
+
+                    if (i == j) //Diagonal Principal
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalPrincipal++;
+                        }
+                    }
+
+                    if ((i + j) == (TamanioGato - 1)) //diagonal inversa
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalInversa++;
+                        }
+                    }
+                }
+            }
+
+            if ((GanoFilas == TamanioGato) || (GanoColumnas == TamanioGato) || (DiagonalInversa == TamanioGato) || (DiagonalPrincipal == TamanioGato))
+            {
+                YaHayGanador = true;
+            }
+
+
+            //Validar cuando sea empate
+            //else
+            //bool Empate =true;
+            //for(var i=0; i< TamanioGato; i++)
+            //{ 
+            //for(var j=0; j< TamanioGato; j++)
+            //{
+            // if()
+            //Empate = false;
+            //}
+
+            //}
+            //if (Empate)
+            //{
+            //    MessageBox.Show("Esto es un empate ¡Reinicia el juego!");
+            //    IniciarJuego();
+            //}
+
+            if (YaHayGanador)
+            {
+                MessageBox.Show("Ya hay ganador");
+                picGanador.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+                if (turno == 1)
+                {
+                    PuntosPlayer1++;
+                    lblplayer1.Text = PuntosPlayer1.ToString();
+                }
+                else
+                {
+                    PuntosPlayer2++;
+                    lblplayer2.Text = PuntosPlayer2.ToString();
+                }
+            }
+
         }
     }
 }
